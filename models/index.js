7e2e343779ee38db431dc21d044dc3389abff92c
Chris McAvoy <chris.mcavoy@gmail.com>
2013-04-28 13:36:53 -0500

    made pages useful but not pretty, changed schema

--- models/index.js
+++ models/index.js
@@ -27,17 +27,25 @@ var RubricItemSchema = new Schema({
   pass: Boolean
 });
 
-var RubricSchema = new Schema({
-  minimum: Number,
-  items: [RubricItemSchema]
-});
-
-
 var SubmissionSchema = new Schema({
   learner: {
     type: String,
     trim: true,
     require: true
+  },
+  name : {
+    type: String,
+    trim: true,
+    require: true
+  },
+  description: {
+    type: String,
+    trim: true,
+    require: false
+  },
+  image: {
+    type: String,
+    require: false
   },
   criteriaUrl: {
     type: String,
@@ -50,12 +58,8 @@ var SubmissionSchema = new Schema({
   },
   evidence: [EvidenceItemSchema],
   rubric: {
-    type: [RubricItemSchema],
-    require: false
-  },
-  evaluation: {
-    type: [RubricItemSchema],
-    require: false
+    minimum: Number,
+    items: [RubricItemSchema]
   }
 });
 
