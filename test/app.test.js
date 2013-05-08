7bab8792ab0391f47a636852a8cb828785404e44
Atul Varma <varmaa@gmail.com>
2013-05-07 21:55:45 -0400

    added nunjucks.

--- test/app.test.js
+++ test/app.test.js
@@ -13,3 +13,35 @@ describe('App', function() {
       .expect(200, done);    
   });
 });
+
+describe('Nunjucks environment', function() {
+  function FakeLoader(map) {
+    return {
+      getSource: function(name) {
+        if (name in map) {
+          return {
+            src: map[name],
+            path: name,
+            upToDate: function() { return true; }
+          };
+        }
+      }
+    };
+  }
+
+  it('should autoescape', function(done) {
+    var app = require('../').app.build({
+      cookieSecret: 'testing'
+    });
+    app.nunjucksEnv.loaders.push(FakeLoader({
+      'test_escaping.html': 'hi {{blah}}'
+    }));
+    app.get('/testing', function(req, res) {
+      res.render('test_escaping.html', {blah: '<script>'});
+    });
+    request(app)
+      .get('/testing')
+      .expect('hi &lt;script&gt;')
+      .expect(200, done);
+  });
+});
