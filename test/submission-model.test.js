48c4ea7c98eec12d52ebb9ad0d297270a1aac3fc
Atul Varma <varmaa@gmail.com>
2013-05-09 11:44:25 -0400

    ensure reviewers of submissions have permission to review.

--- test/submission-model.test.js
+++ test/submission-model.test.js
@@ -1,10 +1,13 @@
 var _ = require('underscore');
 var async = require('async');
 var should = require('should');
+var sinon = require('sinon');
 
 var db = require('./db');
 var baseSubmission = require('./utils').baseSubmission;
-var Submission = require('../').models.Submission;
+var models = require('../').models;
+var Submission = models.Submission;
+var Mentor = models.Mentor;
 
 function ensureInvalid(invalidator) {
   return function(done) {
@@ -22,6 +25,43 @@ function ensureInvalid(invalidator) {
 db.init();
 
 describe('Submission', function() {
+  it('should propagate errors in reviewer validation', function(done) {
+    sinon.stub(Submission.Mentor, 'classificationsFor', function(who, cb) {
+      cb(new Error("oof"));
+    });
+    async.series([
+      db.removeAll(Submission),
+      db.create(Submission, baseSubmission({reviewer: "foo@bar.org"}))
+    ], function(err) {
+      Submission.Mentor.classificationsFor.restore();
+      err.message.should.eql("oof");
+      done();
+    });
+  });
+
+  it('should accept reviewers with proper permissions', function(done) {
+    async.series([
+      db.removeAll(Submission),
+      db.removeAll(Mentor),
+      db.create(Mentor, {email: "foo@bar.org",
+                         classifications: ["math"]}),
+      db.create(Submission, baseSubmission({reviewer: "foo@bar.org"}))
+    ], done);
+  });
+
+  it('should reject reviewers without proper permissions', function(done) {
+    async.series([
+      db.removeAll(Submission),
+      db.removeAll(Mentor),
+      db.create(Submission, baseSubmission({reviewer: "foo@bar.org"}))
+    ], function(err) {
+      err.name.should.eql("ValidationError");
+      err.errors.reviewer.message
+        .should.eql("reviewer does not have permission to review");
+      done();
+    });
+  });
+
   it('should reject unsafe urls for evidence', ensureInvalid(function(attrs) {
     attrs.evidence[1].url = "javascript:lol()";
   }));
