35b822bde8510662078f808f840516e19695c242
Atul Varma <varmaa@gmail.com>
2013-05-10 09:26:15 -0400

    added mentor management functionality to /demo.

--- static/js/demo.js
+++ static/js/demo.js
@@ -1,31 +1,45 @@
 $(window).ready(function() {
   var password = $('#js-password');
-
-  $("#submissions a:first").tab('show');
-  $("form.js-submission").submit(function() {
-    var json = $(this.elements['json']).val();
-
+  var postJSON = function(path, json) {
     try {
       JSON.parse(json);
     } catch (e) {
       alert("Invalid JSON: " + e);
-      return false;
+      return;
     }
 
     $.ajax({
       type: "POST",
-      url: "/api/submit",
+      url: path,
       data: json,
       contentType: 'application/json',
-      username: 'api',
-      password: password.val(),
       success: function(response) {
         alert("Success! See your browser's debug console for details.");
-      },
-      error: function(req) {
-        alert("Error! See your browser's debug console for details.");
       }
     });
+  };
+
+  $("#submissions a:first").tab('show');
+
+  $(document).ajaxSend(function(event, jqxhr, settings) {
+    settings.username = 'api';
+    settings.password = password.val();
+  }).ajaxError(function() {
+    alert("Error! See your browser's debug console for details.");
+  });
+
+  $("#js-update-mentor").click(function() {
+    postJSON("/api/mentors", $("#js-update-mentor-json").val());
+  });
+
+  $("#js-list-mentors").click(function() {
+    $.get('/api/mentors', function(response) {
+      $("#js-mentors").text(JSON.stringify(response, null, 2));
+    });
+  });
+
+  $("form.js-submission").submit(function() {
+    postJSON("/api/submit", $(this.elements['json']).val());
     return false;
   });
 });
