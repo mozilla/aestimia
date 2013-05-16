102d77c6983ca9aa2ddd53e2b62cff232a4b1129
Atul Varma <varmaa@gmail.com>
2013-05-15 22:39:24 -0400

    Added documentation.generateStaticDocs().

--- test/documentation.test.js
+++ test/documentation.test.js
@@ -1,6 +1,17 @@
+var fs = require('fs');
+var path = require('path');
 var should = require('should');
 
 var docs = require('../').documentation;
+
+function nukeDir(tempdir) {
+  if (fs.existsSync(tempdir)) {
+    fs.readdirSync(tempdir).forEach(function(filename) {
+      fs.unlinkSync(path.join(tempdir, filename));
+    });
+    fs.rmdirSync(tempdir);
+  }
+}
 
 describe('documentation utils', function() {
   it('should parse raw api doc sections', function() {
@@ -10,5 +21,14 @@ describe('documentation utils', function() {
     sections[0].title.should.eql('Overview');
     sections[0].html.should.match(/api/i);
     sections[0].html.should.not.match(/<h2>/i);
+  });
+
+  it('should generate static docs', function() {
+    var tempdir = path.join(__dirname, 'tempdocs');
+
+    nukeDir(tempdir);
+    docs.generateStaticDocs(tempdir);
+    fs.existsSync(path.join(tempdir, 'index.html')).should.equal(true);
+    nukeDir(tempdir);
   });
 });
